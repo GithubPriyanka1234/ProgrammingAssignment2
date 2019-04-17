e6318a62dfff4637a88499a1152b43543391c29c
GithubPriyanka1234 <49685279+GithubPriyanka1234@users.noreply.github.com>
2019-04-17 13:32:46 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,8 @@
 ## functions do
 
 ## Write a short comment describing this function
+# makeCacheMatrix creates a list containing a function to (1) set the matrix value, (2) get the matrix value
+#(3)set the inverse matrix, and (4) get the value of of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 m <- NULL
@@ -17,6 +19,9 @@ list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 
 ## Write a short comment describing this function
+#The following function returns the inverse matrix and check to if the value has been computed.
+#If the value has been computed it gets the result and skips the computation.Otherwise, it computes the inverse and sets the value in the cache
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
